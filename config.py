4c79a02848e30fad4a7ab1de1e9a0b1db8b3c346
Makoninio <115080189+Makoninio@users.noreply.github.com>
2025-02-01 10:13:55 -0500

    Update config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     SECRET_KEY = "this-is-a-super-secret-key"
-    OPENAI_KEY = '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'
+    OPENAI_KEY = 'xxxx'
 
 config = {
     'development': DevelopmentConfig,
